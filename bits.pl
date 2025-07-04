66a52a51d00a68028fe15b04e8e8fac9416e263c
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-07-04 14:36:01 +0900

    bits

---
+++ bits.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+use 5.10.0;
+use utf8;
+use strict;
+use warnings;
+use open qw( :utf8 :std );
+use Data::Dumper;
+
+my $number = shift;
+
+printf "%b\n", $number;
+
+
+
+exit;
+
